4bcff2653f9f6e54edb28330ba8e6ce23c53342a
Desmond Lee <lee.desmond2016@gmail.com>
2024-04-29 11:58:38 +0800

    add navbar ui

---
+++ app/static/scripts/userProfileModal.js
@@ -0,0 +1,11 @@
+const modal = document.querySelector("#userProfileModal");
+const openModal = document.querySelector(".open-button");
+const closeModal = document.querySelector(".close-button");
+
+openModal.addEventListener("click", () => {
+  modal.showModal();
+});
+
+closeModal.addEventListener("click", () => {
+  modal.close();
+});
