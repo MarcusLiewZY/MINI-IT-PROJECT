e7d146b789f682c428552f16dcbd124b680789d3
YEAP BOON SHEN <boonshen1159@gmail.com>
2024-05-02 15:20:59 +0800

    link campus page and community guideline page

--- app/main/views.py
+++ app/main/views.py
@@ -27,7 +27,7 @@ def community_guidelines():
 def landing():
     return render_template("main/landing.html")
 
-@main.route("/campus-selection")
+@main.route("/campus-selection", methods = ['GET', 'POST'])
 @login_required
 def campus_selection(): 
     return render_template('campus-selection/campus.html')
