9265fb5dfdc7146c67aef8dccf1620fa420dee4d
YEAP BOON SHEN <boonshen1159@gmail.com>
2024-05-02 15:07:53 +0800

    campus page ui done

--- app/main/views.py
+++ app/main/views.py
@@ -21,6 +21,10 @@ def index():
 def landing():
     return render_template("main/landing.html")
 
+@main.route("/campus-selection")
+@login_required
+def campus_selection(): 
+    return render_template('campus-selection/campus.html')
 
 @main.route("/playground")
 def playground():
--- app/user/views.py
+++ app/user/views.py
@@ -30,7 +30,7 @@ def google_auth():
     if user:
         login_user(user)
         flash("Successfully logged in", "success")
-        return redirect(url_for("main.index"))
+        return redirect(url_for("main.campus_selection"))
     else:
         # check user email endwith mmu.edu.my
         if not user_info["email"].endswith("mmu.edu.my"):
