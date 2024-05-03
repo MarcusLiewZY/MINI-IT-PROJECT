57114bb8a3bda28e927f9c568de7e44f7a32757f
Desmond Lee <lee.desmond2016@gmail.com>
2024-05-03 17:09:13 +0800

    feat: build create post model ui

---
+++ app/static/scripts/createPostModal.js
@@ -0,0 +1,43 @@
+const createPostModal = document.querySelector("#createPostModal");
+const openModalButton = document.querySelector("#openButton");
+const closeModalButton = document.querySelector("#closeButton");
+
+openModalButton.addEventListener("click", () => {
+  createPostModal.showModal();
+});
+
+closeModalButton.addEventListener("click", () => {
+  createPostModal.close();
+});
+
+// Path: app/static/scripts/createPostModal.js
+const contentInput = document.querySelector("#comment");
+
+contentInput.addEventListener("input", () => {
+  contentInput.style.height = "auto";
+  contentInput.style.maxHeight = "200px";
+  contentInput.style.height = `${contentInput.scrollHeight}px`;
+});
+
+const uploadImageIcon = document.querySelector("#uploadImageIcon");
+const imageInput = document.querySelector("#image");
+
+uploadImageIcon.addEventListener("click", () => {
+  imageInput.click();
+});
+
+// console.log the the target filename
+// console.log the file url that will be submitted to the server
+imageInput.addEventListener("change", () => {
+  console.log(imageInput.files[0].name);
+  console.log(URL.createObjectURL(imageInput.files[0]));
+});
+
+// todo: use this method to get the tag color
+// const user_info = JSON.parse(
+//   document.querySelector("#userInfo").dataset.userInfo,
+// );
+
+// for (const key in user_info) {
+//   console.log(`${key}: ${user_info[key]}`);
+// }
