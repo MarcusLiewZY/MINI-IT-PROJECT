3946be8130f60867ff88a9d2bf2805efc3ef04ed
Desmond Lee <lee.desmond2016@gmail.com>
2024-06-02 22:29:18 +0800

    format: rename about folder

--- app/about/views.py
+++ app/about/views.py
@@ -1,11 +1,10 @@
 from flask import render_template
 from flask_login import current_user
-from app.utils.decorators import login_required, require_accept_community_guideline
+from app.utils.decorators import login_required
 from . import about_bp
 
 
 @about_bp.route("/about")
 @login_required
-@require_accept_community_guideline
 def get_about():
     return render_template("about/about.html", user=current_user)
